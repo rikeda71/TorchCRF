ba89a28fb1798f5ee76f3613372f4f27da484b6d
Ryuya Ikeda <rikeda71@gmail.com>
2018-12-05 19:05:11 +0900

    Fix: CRF algorithm

--- TorchCRF/crf.py
+++ TorchCRF/crf.py
@@ -20,9 +20,9 @@ class CRF(nn.Module):
         self.num_labels = num_labels
 
         # 遷移行列の設定
-        # 遷移行列のフォーマット [遷移先, 遷移元]
+        # 遷移行列のフォーマット (遷移元, 遷移先)
         # transition matrix setting
-        # transition matrix format [destination, source]
+        # transition matrix format (source, destination)
         self.trans_matrix = nn.Parameter(self.myTensor(num_labels, num_labels))
         # 先頭と末尾への遷移行列の設定
         # transition matrix of start and end settings
@@ -33,7 +33,6 @@ class CRF(nn.Module):
 
     def initialize_parameters(self) -> None:
         """
-
         initialize transition parameters
         :return: None
         """
@@ -53,8 +52,8 @@ class CRF(nn.Module):
         :return: The log-likelihood (batch_size)
         """
 
-        log_denominator = self._compute_denominator_log_likelihood(h, mask)
         log_numerator = self._compute_numerator_log_likelihood(h, labels, mask)
+        log_denominator = self._compute_denominator_log_likelihood(h, mask)
 
         return log_numerator - log_denominator
 
@@ -63,49 +62,55 @@ class CRF(nn.Module):
         """
 
         compute the denominator term for the log-likelihood
-        :param h: hidden matrix (seq_len, batch_size, num_labels)
-        :param mask: mask tensor of each sequence in mini batch (seq_len, batch_size)
+        :param h: hidden matrix (batch_size, seq_len, num_labels)
+        :param mask: mask tensor of each sequence in mini batch (batch_size, seq_len)
         :return: The score of denominator term for the log-likelihood
         """
 
         batch_size, seq_len, _ = h.size()
+        # 計算できるよう，遷移行列のサイズを変更
+        # (num_labels, num_labels) -> (1, num_labels, num_labels)
+        trans = self.trans_matrix.view(1, self.num_labels, self.num_labels)
         # 先頭から各ラベルへのスコアと各ラベルの1番目のスコアを足し合わせる
         # add the score from beginning to each label and the first score of each label
-        score = self.start_trans.unsqueeze(0) + h[:, 0]
-        # 計算できるよう，遷移行列のサイズを変更
-        # [N, N] -> [1, batch_size, batch_size]
-        trans = self.trans_matrix.unsqueeze(0)
+        score = self.start_trans.view(1, -1) + h[:, 0]
         # ミニバッチ中の単語数だけ処理を行う
         # iterate through processing for the number of words in the mini batch
         for t in range(1, seq_len):
+            # (batch_size, self.num_labels, 1)
+            before_score = score.view(batch_size, self.num_labels, 1)
             # 各系列の系列のt番目のマスクを用意
             # prepare t-th mask of sequences in each sequence
-            mask_t = mask[:, t].unsqueeze(1)
+            # (batch_size, 1)
+            mask_t = mask[:, t].view(batch_size, 1)
             # 各系列におけるt番目の系列ラベルの遷移確率
             # prepare the transition probability of the t-th sequence label in each sequence
-            h_t = h[:, t].unsqueeze(-1)
+            # (batch_size, 1, num_labels)
+            h_t = h[:, t].view(batch_size, 1, self.num_labels)
             # 各系列でのt番目のスコアを導出
             # calculate t-th scores in each sequence
-            score_t = self.logsumexp(score.unsqueeze(1) + h_t + trans)
+            # (batch_size, num_labels)
+            score_t = self.logsumexp(before_score + h_t + trans, 1)
             # スコアの更新
             # update scores
+            # (batch_size, num_labels)
             score = score_t * mask_t + score * (1 - mask_t)
 
         # 末尾のスコアを足し合わせる
         # add the end score of each label
-        score += self.end_trans.unsqueeze(0)
+        score += self.end_trans.view(1, -1)
         # ミニバッチ中のデータ全体の対数尤度を返す
         # return the log likely food of all data in mini batch
-        return self.logsumexp(score)
+        return self.logsumexp(score, 1)
 
     def _compute_numerator_log_likelihood(self, h: torch.FloatTensor,
                                           y: torch.LongTensor,
                                           mask: torch.FloatTensor) -> torch.FloatTensor:
         """
         compute the numerator term for the log-likelihood
-        :param h: hidden matrix (seq_len, batch_size, num_labels)
-        :param y: answer labels of each sequence in mini batch (seq_len, batch_size)
-        :param mask: mask tensor of each sequence in mini batch (seq_len, batch_size)
+        :param h: hidden matrix (batch_size, seq_len, num_labels)
+        :param y: answer labels of each sequence in mini batch (batch_size, seq_len)
+        :param mask: mask tensor of each sequence in mini batch (batch_size, seq_len)
         :return: The score of numerator term for the log-likelihood
         """
 
@@ -119,24 +124,25 @@ class CRF(nn.Module):
 
         for t in range(seq_len - 1):
             mask_t = mask[:, t]
+            mask_t1 = mask[:, t + 1]
             # t+1番目のラベルのスコアを抽出
             # extract the score of t+1 label
-            h_t = torch.cat([h[b, t, y[b, t + 1]] for b in range(batch_size)])
+            # (batch_size)
+            h_t = torch.cat([h[b, t, y[b, t]] for b in range(batch_size)])
             # t番目のラベルからt+1番目のラベルへの遷移スコアを抽出
             # extract the transition score from t-th label to t+1 label
-            trans_t = torch.cat([trans[s[t + 1], s[t]] for s in y])
+            # (batch_size)
+            trans_t = torch.cat([trans[s[t], s[t + 1]] for s in y])
             # 足し合わせる
             # add the score of t+1 and the transition score
-            score += (h_t + trans_t) * mask_t
+            # (batch_size)
+            score += h_t * mask_t + trans_t * mask_t1
 
         # バッチ内の各系列の最後尾のラベル番号を抽出する
         # extract end label number of each sequence in mini batch
+        # (batch_size)
         last_mask_index = mask.long().sum(1) - 1
         last_labels = y.gather(1, last_mask_index.unsqueeze(-1))
-        # 各系列の最後尾のタグからEOSまでのスコアを足し合わせる
-        # Add the scores from the last tag of each sequence to EOS
-        score += self.end_trans[last_labels].view(batch_size)
-
         # hの形を元に戻す
         # restore the shape of h
         h = h.unsqueeze(-1).view(batch_size, seq_len, self.num_labels)
@@ -144,6 +150,9 @@ class CRF(nn.Module):
         # バッチ内の最大長の系列のスコアを足し合わせる
         # Add the score of the sequences of the maximum length in mini batch
         score += h[:, -1].gather(1, last_labels).squeeze(1) * mask[:, -1]
+        # 各系列の最後尾のタグからEOSまでのスコアを足し合わせる
+        # Add the scores from the last tag of each sequence to EOS
+        score += self.end_trans[last_labels].view(batch_size)
 
         return score
 
@@ -151,7 +160,7 @@ class CRF(nn.Module):
                        mask: torch.FloatTensor) -> List[int]:
         """
         decode labels using viterbi algorithm
-        :param h: hidden matrix (seq_len, batch_size, num_labels)
+        :param h: hidden matrix (batch_size, seq_len, num_labels)
         :param mask: mask tensor of each sequence in mini batch (seq_len, batch_size)
         :return: labels of each sequence in mini batch
         """
@@ -168,16 +177,19 @@ class CRF(nn.Module):
         for t in range(1, seq_len):
             # 1つ前の系列のスコアを抽出
             # extract the score of previous sequence
+            # (batch_size, num_labels, 1)
             previous_score = score[t - 1].view(batch_size, -1, 1)
 
             # 系列の隠れ層のスコアを抽出
             # extract the score of hidden matrix of sequence
+            # (batch_size, 1, num_labels)
             h_t = h[:, t].view(batch_size, 1, -1)
 
             # t-1の系列のラベルからtの系列のラベルまでの遷移におけるスコアを抽出
             # self.trans_matrixは系列Aから系列Bまでの遷移のスコアを持っている
             # extract the score in transition from label of t-1 sequence to label of sequence of t
             # self.trans_matrix has the score of the transition from sequence A to sequence B
+            # (batch_size, num_labels, num_labels)
             score_t = previous_score + self.trans_matrix + h_t
 
             # 導出したスコアのうち，各系列の最大値と最大値をとり得る位置を保持
@@ -199,7 +211,7 @@ class CRF(nn.Module):
                                    score: List[torch.FloatTensor],
                                    path: List[torch.LongTensor]) -> List[int]:
         """
-
+        return labels using viterbi algorithm
         :param batch_idx: index of batch
         :param seq_lens: sequence lengths in mini batch (batch_size)
         :param score: transition scores of length max sequence size in mini batch [(batch_size, num_labels)]
@@ -216,21 +228,22 @@ class CRF(nn.Module):
         # viterbiアルゴリズムにより，ラベルを後ろから推定
         # predict labels from back using viterbi algorithm
         for p in reversed(path[: seq_end_idx]):
-            best_last_label = p[batch_idx][best_labels[-1]]
+            best_last_label = p[batch_idx][best_labels[0]]
             best_labels.insert(0, int(best_last_label))
 
         return best_labels
 
     @staticmethod
-    def logsumexp(x: torch.FloatTensor) -> torch.FloatTensor:
+    def logsumexp(x: torch.FloatTensor, dim: int) -> torch.FloatTensor:
         """
-        Returns log(sum(exp(x))) while minimizing the possibility of over/underflow.
+        return log(sum(exp(x))) while minimizing the possibility of over/underflow.
         :param x: the matrix format torch.FloatTensor
+        :param dim: dimensiton
         :return: log(sum(exp(x)))
         """
 
-        max = torch.max(x, -1)[0]
-        return max + torch.log(torch.sum(torch.exp(x - max.unsqueeze(-1)), -1))
+        vmax, _ = x.max(dim)
+        return vmax + torch.log(torch.sum(torch.exp(x - vmax.unsqueeze(dim)), dim))
 
     @staticmethod
     def myTensor(*args) -> torch.FloatTensor:
